1e75684477317904bef83cd680b5d7ffdbf8f157
hight007 <hight_007@hotmail.com>
2023-03-01 21:13:19 +0700

    1.2.5

--- src/components/main/Footer/Footer.js
+++ src/components/main/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="main-footer">
       
       <div className="float-right d-none d-sm-inline-block">
-        <b >Version</b> 1.2.4
+        <b >Version</b> 1.2.5
       </div>
     </footer>
   );
