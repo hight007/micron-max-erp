b4d13e4604381e5904bc233c5dcb29e5ed070200
hight007 <hight_007@hotmail.com>
2023-04-01 11:41:07 +0700

    commit all version

--- src/components/main/Footer/Footer.js
+++ src/components/main/Footer/Footer.js
@@ -1,12 +1,12 @@
 import React from "react";
-
+import version from '../../../../package.json'
 
 const Footer = () => {
   return (
     <footer className="main-footer">
       
       <div className="float-right d-none d-sm-inline-block">
-        <b >Version</b> 1.3.1
+        <b >Version</b> {version.version}
       </div>
     </footer>
   );
