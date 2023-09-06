e2a99cb6ec722b12f33efbacef59e928cd1d2f5a
hight007 <hight_007@hotmail.com>
2023-09-06 19:50:13 +0700

    api url

--- src/constants/index.js
+++ src/constants/index.js
@@ -23,8 +23,8 @@ export const NO = "NO";
 export const OK = "ok";
 export const NOK = "nok";
 
-export const apiUrl = "http://localhost:2008/api/MicronMax/";
-// export const apiUrl = "https://asia-southeast1-micronmax.cloudfunctions.net/micronmax2/api/MicronMax/";
+// export const apiUrl = "http://localhost:2008/api/MicronMax/";
+export const apiUrl = "https://asia-southeast1-micronmax.cloudfunctions.net/micronmax2/api/MicronMax/";
 
 export const apiName = {
   authen: {
